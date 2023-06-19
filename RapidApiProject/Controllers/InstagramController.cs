a2f1b440e50bb89afde61664ff52c096dca6013f
Hakan <dgnhakan1@gmail.com>
2023-06-19 03:27:14 +0300

    .

---
+++ RapidApiProject/Controllers/InstagramController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class InstagramController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(InstagramViewModel model)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://instagram-scraper-2022.p.rapidapi.com/ig/info_username/?user={model.user.username}"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "f874e08470msh3574dfb2a70fff4p1e1129jsn1122a39cc826" },
+        { "X-RapidAPI-Host", "instagram-scraper-2022.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var Data = JsonConvert.DeserializeObject<InstagramViewModel>(body);
+                ViewBag.Follower = Data.user.follower_count;
+                ViewBag.Following = Data.user.following_count;
+                return View(Data);
+            }
+        }
+    }
+}
---
+++ RapidApiProject/Models/InstagramViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RapidApiProject.Models
+{
+    public class InstagramViewModel
+    {
+
+      
+            public User user { get; set; }
+
+
+        public class User
+        {
+            public int follower_count { get; set; }
+            public int following_count { get; set; }
+            public string username { get; set; }
+
+        }
+     
+    }
+}
